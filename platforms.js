faa1e1f5ae3bc16a400d576b9045282366d2089b
dtlines <88255984+dtlines@users.noreply.github.com>
2022-05-27 09:27:37 -0400

    Create platforms.js

---
+++ platforms.js
@@ -0,0 +1,60 @@
+
+
+// Sort array by id in asc/desc order
+
+posts.sort((b, a) => parseFloat(a.id) - parseFloat(b.id));
+
+// Create the template literal
+
+const markup = `
+<ul>
+    ${posts.map(post => 
+            `
+
+            <li>
+              <div class="heading">
+                  <h1><i>${post.title}</i></h1>
+               <span class="date">
+                  ${post.date}
+                </span>
+              </div>
+             <center>
+             <img style="width:50%" src=${post.image}>
+             </center>
+                <p>${post.desc}</p>
+             </li>
+             <hr></hr>
+              `
+     ).join('')}
+ </ul>
+`;
+
+// Insert the template literal into the DOM
+
+blog.innerHTML = markup
+
+// COPY BUTTONS START
+var copy_text_val = document.querySelector('.copy_text');
+
+function Copy() {
+  copy_text_val.select();
+  document.execCommand("copy");
+  console.log(copy_text_val.value);
+} 
+
+var copy_text_val2 = document.querySelector('.copy_text2');
+
+function Copy2() {
+  copy_text_val2.select();
+  document.execCommand("copy");
+  console.log(copy_text_val.value);
+} 
+
+var copy_text_val3 = document.querySelector('.copy_text3');
+
+function Copy3() {
+  copy_text_val3.select();
+  document.execCommand("copy");
+  console.log(copy_text_val.value);
+} 
+// COPY BUTTONS END
